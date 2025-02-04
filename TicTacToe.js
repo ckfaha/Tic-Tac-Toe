7046962e7a5335d7e3e5c42923fc3915db5123e4
ckfaha <mf725702@gmail.com>
2025-02-04 12:20:21 +0530

    first commit

---
+++ TicTacToe.js
@@ -0,0 +1,16 @@
+//1 Deposit the money
+//2 determine the no of line they bet
+//3 Collect a bet amount
+//4 Spin the slot machine
+//5 check if the user won
+//6 give the winning money to the user
+//7 play again
+
+const prompt=require("prompt-sync")();
+
+const deposit=()=>{
+
+    const depositAmount=prompt("Enter A Deposit Amount :")
+
+};
+deposit();
